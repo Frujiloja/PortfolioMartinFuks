b4169cffe4cc74df81c20cc01e2620e4d0907839
Martin Fuks <martinfukss@gmail.com>
2024-08-20 16:51:08 -0300

    Navbar

---
+++ Portfolio/src/components/Navbar.jsx
@@ -0,0 +1,69 @@
+import React, { useState } from "react";
+import styles from "../components/Navbar.module.css";
+
+export const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  return (
+    <div className={styles.navbar}>
+      <nav className={styles.desktopnav}>
+        <div className={styles.logo}>Martin Fuks</div>
+        <div className={styles.menu}>
+          <ul className={styles.menuitems}>
+            <li>
+              <a href="#about">About</a>
+            </li>
+            <li>
+              <a href="#experience">Experience</a>
+            </li>
+            <li>
+              <a href="#projects">Projects</a>
+            </li>
+            <li>
+              <a href="#contact">Contact</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      <nav className={styles.burguernav}>
+        <div className={styles.logo}>Martin Fuks</div>
+        <div className={styles.burguermenu}>
+          <div
+            className={`${styles.burguericon} ${menuOpen ? styles.open : ""}`}
+            onClick={toggleMenu}
+          >
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+          <ul className={`${styles.menulinks} ${menuOpen ? styles.open : ""}`}>
+            <li>
+              <a href="#about" onClick={toggleMenu}>
+                About
+              </a>
+            </li>
+            <li>
+              <a href="#experience" onClick={toggleMenu}>
+                Experience
+              </a>
+            </li>
+            <li>
+              <a href="#projects" onClick={toggleMenu}>
+                Projects
+              </a>
+            </li>
+            <li>
+              <a href="#contact" onClick={toggleMenu}>
+                Contact
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+  );
+};
