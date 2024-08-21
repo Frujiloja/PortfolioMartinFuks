346129252a467b8ff09c24e9905c74eca0f90821
Martin Fuks <martinfukss@gmail.com>
2024-08-21 17:43:59 -0300

    Projects

---
+++ Portfolio/src/components/Projects.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import styles from "./Projects.module.css";
+
+export const Projects = () => {
+  return (
+    <div className={styles.projectscontainer}>
+      <p className={styles.p1}>Browse My Recent</p>
+      <h1 className={styles.title}>Projects</h1>
+      <div className={styles.projects}>
+        <div className={styles.projectcolor}>
+          <div className={styles.articlecontainer}>
+            <img
+              src="project-1.png"
+              alt="project 1"
+              className={styles.projectimage}
+            />
+          <h2 className={styles.subtitle}>Project One</h2>
+          <div className={styles.btncontainer}>
+            <button
+              className={styles.btncolor}
+              onClick={() => (location.href = "https://github.com/Frujiloja/")}
+            >
+              Github
+            </button>
+          </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
