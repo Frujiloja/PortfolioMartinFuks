b992a1deffb1e6ba7b2c80ff6e15352cc271e8f6
Martin Fuks <martinfukss@gmail.com>
2024-09-05 23:17:57 -0300

    responsive

--- Portfolio/src/components/Profile.jsx
+++ Portfolio/src/components/Profile.jsx
@@ -1,13 +1,9 @@
 import React from "react";
 import styles from "./Profile.module.css";
 
-
 export const Profile = () => {
   return (
     <div id="profile" className={styles.profilesection}>
-      <div className={styles.pic}>
-        <img src="perfil.png" alt="Martin Fuks profile picture" />
-      </div>
       <div className={styles.text}>
         <p className={styles.textp1}>Hello, I'm</p>
         <h1 className={styles.title}>Martin Fuks</h1>
@@ -33,7 +29,9 @@ export const Profile = () => {
             className={styles.icon}
             onClick={() =>
               window.open(
-                "https://www.linkedin.com/in/martin-fuks-251b91141/", "_blank")
+                "https://www.linkedin.com/in/martin-fuks-251b91141/",
+                "_blank"
+              )
             }
           />
           <img
@@ -46,7 +44,19 @@ export const Profile = () => {
           />
         </div>
       </div>
-      <img src="arrow c.png" alt="arrow pic" className={styles.iconarrow} onClick={() => document.getElementById('about').scrollIntoView({ behavior: 'smooth' })} />
+      <div className={styles.pic}>
+        <img src="perfil.png" alt="Martin Fuks profile picture" />
+      </div>
+      <img
+        src="arrow c.png"
+        alt="arrow pic"
+        className={styles.iconarrow}
+        onClick={() =>
+          document
+            .getElementById("about")
+            .scrollIntoView({ behavior: "smooth" })
+        }
+      />
     </div>
   );
 };
--- Portfolio/src/components/Projects.jsx
+++ Portfolio/src/components/Projects.jsx
@@ -4,9 +4,15 @@ import styles from "./Projects.module.css";
 export const Projects = () => {
   const [currentImage1, setCurrentImage1] = useState(0);
   const [currentImage2, setCurrentImage2] = useState(0);
+  const [currentImage3, setCurrentImage3] = useState(0);
+  const [currentImage4, setCurrentImage4] = useState(0);
+  const [currentImage5, setCurrentImage5] = useState(0);
 
   const images1 = ["1.jpg", "2.jpg", "3.jpg", "4.jpg"];
-  const images2 = ["5.jpg", "6.jpg"];
+  const images2 = ["5.jpg", "6.jpg", "7.jpg"];
+  const images3 = ["8.jpg", "9.jpg", "10.jpg"];
+  const images4 = ["11.jpg", "12.jpg", "13.jpg"];
+  const images5 = ["14.jpg", "15.jpg"];
 
   const nextImage1 = () => {
     setCurrentImage1((prevIndex) => (prevIndex + 1) % images1.length);
@@ -28,6 +34,36 @@ export const Projects = () => {
     );
   };
 
+  const nextImage3 = () => {
+    setCurrentImage3((prevIndex) => (prevIndex + 1) % images3.length);
+  };
+
+  const prevImage3 = () => {
+    setCurrentImage3((prevIndex) =>
+      prevIndex === 0 ? images3.length - 1 : prevIndex - 1
+    );
+  };
+
+  const nextImage4 = () => {
+    setCurrentImage4((prevIndex) => (prevIndex + 1) % images4.length);
+  };
+
+  const prevImage4 = () => {
+    setCurrentImage4((prevIndex) =>
+      prevIndex === 0 ? images4.length - 1 : prevIndex - 1
+    );
+  };
+
+  const nextImage5 = () => {
+    setCurrentImage5((prevIndex) => (prevIndex + 1) % images5.length);
+  };
+
+  const prevImage5 = () => {
+    setCurrentImage5((prevIndex) =>
+      prevIndex === 0 ? images5.length - 1 : prevIndex - 1
+    );
+  };
+
   return (
     <div id="projects" className={styles.projectscontainer}>
       <p className={styles.p1}>Browse My Recent</p>
@@ -41,16 +77,15 @@ export const Projects = () => {
               alt={`project ${currentImage1 + 1}`}
               className={styles.projectimage}
             />
-            <h2 className={styles.subtitle}>Project One</h2>
+            {/* <h2 className={styles.subtitle}>Inox Style</h2> */}
+            <p className={styles.p}>
+              Web Design, Layout, and Development for an Automotive Services and
+              Products Company.
+            </p>
             <div className={styles.btncontainer}>
               <button className={styles.prevBtn} onClick={prevImage1}>
                 {"<"}
               </button>
-              <button className={styles.nextBtn} onClick={nextImage1}>
-                {">"}
-              </button>
-            </div>
-            <div className={styles.btncontainer}>
               <button
                 className={styles.btncolor}
                 onClick={() =>
@@ -58,6 +93,9 @@ export const Projects = () => {
                 }
               >
                 Visit Website
+              </button>
+              <button className={styles.nextBtn} onClick={nextImage1}>
+                {">"}
               </button>
             </div>
           </div>
@@ -70,23 +108,25 @@ export const Projects = () => {
               alt={`project 2`}
               className={styles.projectimage}
             />
-            <h2 className={styles.subtitle}>Project Two</h2>
+            {/* <h2 className={styles.subtitle}>Strings & Keys</h2> */}
+            <p className={styles.p}>
+              Ecommerce Development and Design for a Musical Instrument Store.
+              Front, Back & DB.
+            </p>
             <div className={styles.btncontainer}>
               <button className={styles.prevBtn} onClick={prevImage2}>
                 {"<"}
               </button>
-              <button className={styles.nextBtn} onClick={nextImage2}>
-                {">"}
-              </button>
-            </div>
-            <div className={styles.btncontainer}>
               <button
                 className={styles.btncolor}
                 onClick={() =>
                   window.open("https://example.com/project2", "_blank")
                 }
               >
-                Visit Website
+                Github
+              </button>
+              <button className={styles.nextBtn} onClick={nextImage2}>
+                {">"}
               </button>
             </div>
           </div>
@@ -95,27 +135,89 @@ export const Projects = () => {
         <div className={styles.projectcolor}>
           <div className={styles.articlecontainer}>
             <img
-              src={images2[currentImage2]}
+              src={images3[currentImage3]}
               alt={`project 3`}
               className={styles.projectimage}
             />
-            <h2 className={styles.subtitle}>Project Three</h2>
+            {/* <h2 className={styles.subtitle}>Wonder Toys</h2> */}
+            <p className={styles.p}>
+              Ecommerce Development and Design for a Toys Store. Front, Back &
+              DB.
+            </p>
             <div className={styles.btncontainer}>
-              <button className={styles.prevBtn} onClick={prevImage2}>
+              <button className={styles.prevBtn} onClick={prevImage3}>
                 {"<"}
               </button>
-              <button className={styles.nextBtn} onClick={nextImage2}>
-                {">"}
-              </button>
-            </div>
-            <div className={styles.btncontainer}>
               <button
                 className={styles.btncolor}
                 onClick={() =>
                   window.open("https://example.com/project3", "_blank")
                 }
               >
-                Visit Website
+                Github
+              </button>
+              <button className={styles.nextBtn} onClick={nextImage3}>
+                {">"}
+              </button>
+            </div>
+          </div>
+        </div>
+        {/* Card 4 */}
+        <div className={styles.projectcolor}>
+          <div className={styles.articlecontainer}>
+            <img
+              src={images4[currentImage4]}
+              alt={`project 4`}
+              className={styles.projectimage}
+            />
+            {/* <h2 className={styles.subtitle}>Wonder Toys</h2> */}
+            <p className={styles.p}>
+            Simple Website Design and Development for a Sneakers Company.
+            </p>
+            <div className={styles.btncontainer}>
+              <button className={styles.prevBtn} onClick={prevImage4}>
+                {"<"}
+              </button>
+              <button
+                className={styles.btncolor}
+                onClick={() =>
+                  window.open("https://example.com/project3", "_blank")
+                }
+              >
+                Github
+              </button>
+              <button className={styles.nextBtn} onClick={nextImage4}>
+                {">"}
+              </button>
+            </div>
+          </div>
+        </div>
+        {/* Card 5 */}
+        <div className={styles.projectcolor}>
+          <div className={styles.articlecontainer}>
+            <img
+              src={images5[currentImage5]}
+              alt={`project 5`}
+              className={styles.projectimage}
+            />
+            {/* <h2 className={styles.subtitle}>Wonder Toys</h2> */}
+            <p className={styles.p}>
+            Simple Website Design and Development for a Bio Page.
+            </p>
+            <div className={styles.btncontainer}>
+              <button className={styles.prevBtn} onClick={prevImage5}>
+                {"<"}
+              </button>
+              <button
+                className={styles.btncolor}
+                onClick={() =>
+                  window.open("https://example.com/project3", "_blank")
+                }
+              >
+                Github
+              </button>
+              <button className={styles.nextBtn} onClick={nextImage5}>
+                {">"}
               </button>
             </div>
           </div>
