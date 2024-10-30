e3029813fe151774cfd6abc39e6afe329a523923
Martin Fuks <martinfukss@gmail.com>
2024-10-30 18:56:29 -0300

    footer

--- Portfolio/src/components/Footer.jsx
+++ Portfolio/src/components/Footer.jsx
@@ -6,10 +6,8 @@ const Footer = () => {
     <footer className={styles.footer}>
       <div className={styles.container}>
         <p className={styles.text}>
-          © 2024 Martin Fuks | <a href="mailto:martinfukss@gmail.com" className={styles.contact}>Contacto</a> | 
-          <a href="https://facebook.com" target="_blank" rel="noopener noreferrer" className={styles.link}>Facebook</a> | 
-          <a href="https://twitter.com" target="_blank" rel="noopener noreferrer" className={styles.link}>Twitter</a> | 
-          <a href="https://instagram.com" target="_blank" rel="noopener noreferrer" className={styles.link}>Instagram</a>
+          © 2024 Powered By Martin Fuks | <a href="mailto:martinfukss@gmail.com" className={styles.contact}>Contact</a> | 
+          <a href="https://api.whatsapp.com/send/?phone=5491157719725&type=phone_number&app_absent=0" target="_blank" rel="noopener noreferrer" className={styles.link}>1157719725</a> | 
         </p>
       </div>
     </footer>
